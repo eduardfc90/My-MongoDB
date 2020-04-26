cab07c558768a86b8bbfe292a74304f78e54e81f
eduardfc90 <eduard.fercho@gmail.com>
2020-04-26 01:40:34 +0000

    saving datos in BD and printing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send('Recibido');
+const Task = require('./model/task');
+
+// settings
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.render('index', {tasks });
 })
+
+// Create Task
+app.post('/create', async (req, res) => {
+    const newTask = new Task({
+        task: req.body.task,
+        description: req.body.description
+    });
+    await newTask.save();
+    res.redirect('/');
+})
+
+// STATIC Files
+app.use(express.static(__dirname + '/public'));
 
 module.exports = app;
